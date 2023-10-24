260abbe031a645ca1f882ad179148ac14b935f33
Den <den.belotserkovets@gmail.com>
2023-10-24 16:15:46 +0300

    fix: pages

--- src/routes/elements/[element]/index.tsx
+++ src/routes/elements/[element]/index.tsx
@@ -1,41 +1,48 @@
-import { component$, Resource, useResource$ } from "@builder.io/qwik";
-import { useLocation } from "@builder.io/qwik-city";
+import { component$ } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
 import {
-  getContent,
-  RenderContent,
   getBuilderSearchParams,
+  Content,
+  fetchOneEntry,
 } from "@builder.io/sdk-qwik";
 import { CUSTOM_COMPONENTS } from "~/routes";
 
 export const BUILDER_PUBLIC_API_KEY = "1081fc3ead3540d68ef4330cdac286ad";
 export const BUILDER_MODEL = "element-page";
 
-export default component$(() => {
-  const location = useLocation();
+export const useBuilderContent = routeLoader$(async ({ url, error }) => {
+  const isPreviewing = url.searchParams.has("builder.preview");
 
-  const builderContentRsrc = useResource$<any>(() => {
-    return getContent({
-      model: BUILDER_MODEL,
-      apiKey: BUILDER_PUBLIC_API_KEY,
-      options: getBuilderSearchParams(location.query),
-      userAttributes: {
-        urlPath: location.pathname || "/",
-      },
-    });
+  const builderContent = await fetchOneEntry({
+    model: BUILDER_MODEL,
+    apiKey: BUILDER_PUBLIC_API_KEY,
+    options: getBuilderSearchParams(url.searchParams),
+    userAttributes: {
+      urlPath: url.pathname,
+    },
   });
 
+  // If there's no content, throw a 404.
+  // You can use your own 404 component here
+  if (!builderContent && !isPreviewing) {
+    throw error(404, "Page not found");
+  }
+  // return content fetched from Builder, which is JSON
+  return builderContent;
+});
+
+export default component$(() => {
+  const content = useBuilderContent();
+
+  // RenderContent uses `content` to
+  // render the content of the given model, here a page,
+  // of your space (specified by the API Key)
   return (
-    <Resource
-      value={builderContentRsrc}
-      onPending={() => <div>Loading...</div>}
-      onResolved={(content) => (
-        <RenderContent
-          model={BUILDER_MODEL}
-          content={content}
-          apiKey={BUILDER_PUBLIC_API_KEY}
-          customComponents={CUSTOM_COMPONENTS}
-        />
-      )}
+    <Content
+      model={BUILDER_MODEL}
+      content={content.value}
+      apiKey={BUILDER_PUBLIC_API_KEY}
+      customComponents={CUSTOM_COMPONENTS}
     />
   );
 });
